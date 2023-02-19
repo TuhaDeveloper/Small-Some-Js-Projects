129d0ff7cd86207e3471b0629e68fcb7448171ce
jahidul islam <jahid2005tuha@gmail.com>
2023-02-19 21:54:32 +0600

    some javasCript project first added

---
+++ index.js
@@ -0,0 +1,100 @@
+
+
+//================ input value from user and outpute total sum with value =============== //
+
+const first = new Array();
+
+var sum = 0;
+
+for (var i = 0; i < 5; i++) {
+    first[i] = parseInt(prompt("enter you number"))
+    console.log(first[i])
+    sum = sum + first[i]
+}
+
+console.log('total sum is = ' + sum)
+
+
+//======================= find maximum number from array and return output maximum number =============== //
+
+
+function highscoreNumber(score) {
+    var max = score[0];
+    for (var i = 1; i < score.length; i++) {
+        if (max < score[i]) {
+            max = score[i]
+        }
+    }
+    return max;
+}
+
+const scores = [12, 34, 905, 98, 23, 103, 506];
+
+const Result = highscoreNumber(scores)
+console.log(Result)
+
+
+//= create a function that will recive 2d dimantional array and return the players name who has maximum number =============== //
+
+
+function higherRunScorer(highScorer) {
+
+    var maxArray = highScorer[0][0]
+    var maxArraynum = highScorer[0][1]
+
+    for (var i = 1; i < highScorer.length; i++) {
+        if (maxArraynum < highScorer[i][1]) {
+            maxArraynum = highScorer[i][1];
+            maxArray = highScorer[i][0];
+        }
+    }
+    return maxArray;
+
+}
+
+const higherRunScorers = [
+    ["jahid", 23],
+    ["efran", 3],
+    ["borhan", 34]
+]
+
+const result = higherRunScorer(higherRunScorers)
+console.log(result)
+
+//============= create a image slider project =====================
+
+
+const photos = ["Images/tab-content-2-1.png", "Images/tab-content-2-2.png", "Images/tab-content-2-3.png"];
+const image = document.querySelector('img');
+
+count = 0;
+
+
+function next() {
+    count++;
+    if (count >= photos.length) {
+        count = 0;
+        image.src = photos[count]
+    } else {
+        image.src = photos[count]
+    }
+};
+
+function prev() {
+    count--;
+
+    if (count < 0) {
+        count = photos.length - 1;
+        image.src = photos[count];
+    } else {
+        image.src = photos[count]
+    }
+
+
+}
+
+//========== create a project if i click then audio will play and add some animation with keypress event ========
+
+// it's comeing
+
+
